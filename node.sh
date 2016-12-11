2095467d6479a52e5c5f24ef753c4b46fcbb0a0a
Jam Risser <jamrizzi@gmail.com>
2016-12-11 12:22:12 -0600

    Temporarily disabled mounting external

--- node.sh
+++ node.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # settings
-DATA_DIRECTORY=/exports/
-VOLUME_MOUNT=/dev/disk/by-id/google-disk-1
+# DATA_DIRECTORY=/exports/
+# VOLUME_MOUNT=/dev/disk/by-id/google-disk-1
 MANAGEMENT_NODE=node01
 STORAGE_SERVICE_ID=3
 STORAGE_TARGET_ID=301
@@ -11,14 +11,14 @@ KERNEL_MODULE_AUTOBUILD=N
 if [ $(whoami) = "root" ]; then # if run as root
 
 # gather information
-read -p "Data Directory ($DATA_DIRECTORY): " DATA_DIRECTORY_NEW
-if [ $DATA_DIRECTORY_NEW ]; then
-    DATA_DIRECTORY=$DATA_DIRECTORY_NEW
-fi
-read -p "Volume Mount ($VOLUME_MOUNT): " VOLUME_MOUNT_NEW
-if [ $VOLUME_MOUNT_NEW ]; then
-    VOLUME_MOUNT=$VOLUME_MOUNT_NEW
-fi
+# read -p "Data Directory ($DATA_DIRECTORY): " DATA_DIRECTORY_NEW
+# if [ $DATA_DIRECTORY_NEW ]; then
+#     DATA_DIRECTORY=$DATA_DIRECTORY_NEW
+# fi
+# read -p "Volume Mount ($VOLUME_MOUNT): " VOLUME_MOUNT_NEW
+# if [ $VOLUME_MOUNT_NEW ]; then
+#     VOLUME_MOUNT=$VOLUME_MOUNT_NEW
+# fi
 read -p "Management Node ($MANAGEMENT_NODE): " MANAGEMENT_NODE_NEW
 if [ $MANAGEMENT_NODE_NEW ]; then
     MANAGEMENT_NODE=$MANAGEMENT_NODE_NEW
@@ -40,17 +40,17 @@ fi
 yum update -y
 
 # mount and create data directory
-mkfs.xfs -i size=512 $VOLUME_MOUNT
-mkdir -p $DATA_DIRECTORY
-echo "$VOLUME_MOUNT $DATA_DIRECTORY xfs defaults 1 2" >> /etc/fstab
-mount -a && mount
-chmod -R 777 $DATA_DIRECTORY
+# mkfs.xfs -i size=512 $VOLUME_MOUNT
+# mkdir -p $DATA_DIRECTORY
+# echo "$VOLUME_MOUNT $DATA_DIRECTORY xfs defaults 1 2" >> /etc/fstab
+# mount -a && mount
+# chmod -R 777 $DATA_DIRECTORY
 
 # install storage server
 (echo $MANAGEMENT_NODE; echo $STORAGE_SERVICE_ID; echo $STORAGE_TARGET_ID) | curl -o storage-install.sh https://raw.githubusercontent.com/jamrizzi/beegfs-docker/master/storage-install.sh
 bash storage-install.sh
 rm storage-install.sh
-/beegfs-storage status
+/etc/init.d/beegfs-storage status
 
 # install client server
 (echo $MANAGEMENT_NODE; echo $KERNEL_MODULE_AUTOBUILD; echo N) | curl -o client-install.sh https://raw.githubusercontent.com/jamrizzi/beegfs-docker/master/client-install.sh
