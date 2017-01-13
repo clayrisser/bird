14fdbba815107736c675ced42b114b4cb8d5abd5
Jam Risser <jam@jamrizzi.com>
2017-01-12 23:25:48 -0600

    Updated support for ident

--- src/master.py
+++ src/master.py
@@ -24,13 +24,12 @@ def get_defaults():
         'rancher_domain': 'cloud.yourdomain.com',
         'management_node': 'node01',
         'metadata_service_id': '1',
-        'duplicity_target_url': 'imabucket/backup',
-        'gs_access_key_id': 'gs-access-key-id',
-        'gs_secret_access_key': 'gs-secret-access-key',
-        'cron_schedule': '0 0 0 * * *',
-        'allow_source_mismatch': 'false',
-        'volumes_directory': '/volumes',
         'volumes_mount': 'local',
+        'cron_schedule': '0 0 0 * * *',
+        'backup_storage_volume': '/backup',
+        'backup_storage_target_url': '',
+        'backup_storage_access_key': '',
+        'backup_storage_secret_key': '',
         'rancher_mysql_database': 'rancher',
         'mysql_root_password': 'hellodocker'
     }
@@ -41,13 +40,12 @@ def gather_information(defaults):
     options['rancher_domain'] = default_prompt('Rancher Domain', defaults['rancher_domain'])
     options['management_node'] = default_prompt('Management Node', defaults['management_node'])
     options['metadata_service_id'] = default_prompt('Metadata Service ID', defaults['metadata_service_id'])
-    options['duplicity_target_url'] = default_prompt('Duplicity Target URL', defaults['duplicity_target_url'])
-    options['gs_access_key_id'] = default_prompt('Google Storage Access Key ID', defaults['gs_access_key_id'])
-    options['gs_secret_access_key'] = default_prompt('Google Storage Secret Access Key', defaults['gs_secret_access_key'])
-    options['cron_schedule'] = default_prompt('Cron Schedule', defaults['cron_schedule'])
-    options['allow_source_mismatch'] = default_prompt('Allow Source Mismatch', defaults['allow_source_mismatch'])
-    options['volumes_directory'] = default_prompt('Volumes Directory', defaults['volumes_directory'])
     options['volumes_mount'] = default_prompt('Volumes Mount', defaults['volumes_mount'])
+    options['cron_schedule'] = default_prompt('Cron Schedule', defaults['cron_schedule'])
+    options['backup_storage_volume'] = default_prompt('Backup Storage Volume', defaults['backup_storage_volume'])
+    options['backup_storage_target_url'] = default_prompt('Backup Storage Target URL', defaults['backup_storage_target_url'])
+    options['backup_storage_access_key'] = default_prompt('Backup Storage Access Key', defaults['backup_storage_access_key'])
+    options['backup_storage_secret_key'] = default_prompt('Backup Storage Secret Key', defaults['backup_storage_secret_key'])
     options['rancher_mysql_database'] = default_prompt('Rancher Mysql Database', defaults['rancher_mysql_database'])
     options['mysql_root_password'] = default_prompt('MYSQL Root Password', defaults['mysql_root_password'])
     return options
@@ -61,10 +59,10 @@ def default_prompt(name, fallback):
         return fallback
 
 def create_volumes_dir(options):
-    os.system('mkdir -p ' + options['volumes_directory'])
+    os.system('mkdir -p /volumes')
     if os.system(options['volumes_mount']):
         os.system('mkfs.xfs -i size=512 ' + options['volumes_mount'])
-        os.system('echo ' + options['volumes_mount'] + options['volumes_directory'] + ' xfs defaults 1 2" >> /etc/fstab')
+        os.system('echo "' + options['volumes_mount'] + ' /volumes xfs defaults 1 2" >> /etc/fstab')
         os.system('mount -a && mount')
 
 def prepare_system():
@@ -75,12 +73,11 @@ def install_rancher(options):
     curl -L https://raw.githubusercontent.com/jamrizzi/rancher-ident/master/scripts/download.sh | bash
     (echo ''' + options['email'] + '''; \
     echo ''' + options['rancher_domain'] + '''; \
-    echo ''' + options['duplicity_target_url'] + '''; \
-    echo ''' + options['gs_access_key_id'] + '''; \
-    echo ''' + options['gs_secret_access_key'] + '''; \
+    echo ''' + options['backup_storage_volume'] + '''; \
+    echo ''' + options['backup_storage_target_url'] + '''; \
+    echo ''' + options['backup_storage_access_key'] + '''; \
+    echo ''' + options['backup_storage_secret_key'] + '''; \
     echo ''' + options['cron_schedule'] + '''; \
-    echo ''' + options['allow_source_mismatch'] + '''; \
-    echo ''' + options['volumes_directory'] + '''; \
     echo ''' + options['volumes_mount'] + '''; \
     echo ''' + options['rancher_mysql_database'] + '''; \
     echo ''' + options['mysql_root_password'] + ''') | ./rancher-ident
