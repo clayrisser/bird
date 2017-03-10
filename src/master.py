22e334cdbd292683311f56a578b756995979afed
Jam Risser <jam@jamrizzi.com>
2017-03-10 14:32:57 -0600

    Changed backup_cloud terminology

--- src/master.py
+++ src/master.py
@@ -12,7 +12,7 @@ def main():
     options = gather_information(get_defaults())
     helper.prepare()
     prepare_system()
-    setup_backup_cloud(options)
+    setup_cloud_backup(options)
     install_rancher(options)
     install_beegfs_management(options)
     install_beegfs_metadata(options)
@@ -25,7 +25,7 @@ def get_defaults():
         'master_domain': 'cloud.yourdomain.com',
         'metadata_service_id': '2',
         'cron_schedule': '0 0 * * *',
-        'backup_cloud_mount': 'local',
+        'cloud_backup_mount': 'local',
         'volumes_mount': 'local',
         'volumes_backup_mount': 'local',
         'rancher_mysql_database': 'rancher',
@@ -38,9 +38,9 @@ def gather_information(defaults):
     options['master_domain'] = helper.default_prompt('Master Domain', defaults['master_domain'])
     options['metadata_service_id'] = helper.default_prompt('Metadata Service ID', defaults['metadata_service_id'])
     options['cron_schedule'] = helper.default_prompt('Cron Schedule', defaults['cron_schedule'])
-    options['backup_cloud_mount'] = helper.default_prompt('Backup Cloud Mount', defaults['backup_cloud_mount'])
+    options['cloud_backup_mount'] = helper.default_prompt('Cloud Backup Mount', defaults['backup_cloud_mount'])
     options['volumes_mount'] = helper.default_prompt('Volumes Mount', defaults['volumes_mount'])
-    options['volumes_backup_mount'] = helper.default_prompt('Backup Volumes Mount', defaults['volumes_backup_mount'])
+    options['volumes_backup_mount'] = helper.default_prompt('Volumes Backup Mount', defaults['volumes_backup_mount'])
     options['rancher_mysql_database'] = helper.default_prompt('Rancher Mysql Database', defaults['rancher_mysql_database'])
     options['mysql_root_password'] = helper.default_prompt('MYSQL Root Password', defaults['mysql_root_password'])
     return options
@@ -65,8 +65,8 @@ def prepare_system():
         print('Operating system not supported')
         sys.exit('Exiting installer')
 
-def setup_backup_cloud(options):
-    helper.mount(options['backup_cloud_mount'], '/exports/cloud-backup')
+def setup_cloud_backup(options):
+    helper.mount(options['cloud_backup_mount'], '/exports/cloud-backup')
     os.system('''
     chmod 777 /exports/cloud-backup
     echo "/exports/cloud-backup    *(rw,sync,no_subtree_check)" | tee -a /etc/exports
